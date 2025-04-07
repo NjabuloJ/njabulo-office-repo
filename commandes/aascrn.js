447ae23bc3f12acbe0a45e638afb9f016cd1982e
NjabuloJ <fanajb65@gmail.com>
2025-04-07 12:14:41 +0200

    Update aascrn.js

--- commandes/aascrn.js
+++ commandes/aascrn.js
@@ -3,7 +3,7 @@ const gis = require('g-i-s');
 const axios = require('axios');
 const conf = require(__dirname + '/../set');
 zokou({
-  nomCom: "screenswidth",
+  nomCom: "screenshots",
   categorie: "take-screenshots",
   reaction: "🎞️"
 }, async (dest, zk, commandeOptions) => {
@@ -11,7 +11,7 @@ zokou({
 
   try {
     // Caption for the screenshot
-    const cap = `*Screenshot taken by Lucky  Md*`;
+    const cap = `*Screenshot taken by Njabulo Jb*`;
 
     // Check if a URL is provided
     if (!arg[0]) {
@@ -34,7 +34,7 @@ zokou({
   }
 });
 zokou({
-  nomCom: "screenscrop",
+  nomCom: "screenshots-noan",
   categorie: "take-screenshots",
   reaction: "🎞️"
 }, async (dest, zk, commandeOptions) => {
@@ -42,132 +42,7 @@ zokou({
 
   try {
     // Caption for the screenshot
-    const cap = `*Screenshot taken by Lucky Md*`;
-
-    // Check if a URL is provided
-    if (!arg[0]) {
-      return repondre('Please insert a website link to take a screenshot!');
-    }
-
-    // Construct the screenshot URL
-    const image = `https://image.thum.io/get/crop/${arg[0]}`;
-
-    // Send the screenshot image with the caption
-    await zk.sendMessage(dest, {
-      image: { url: image },
-      caption: cap
-    }, { quoted: ms });
-
-  } catch (error) {
-    // Log the error and notify the user with a formatted error message
-    console.error(error);
-    repondre(`An error occurred while processing the screenshot: ${error.message}`);
-  }
-});
-zokou({
-  nomCom: "maxage",
-  categorie: "take-screenshots",
-  reaction: "🎞️"
-}, async (dest, zk, commandeOptions) => {
-  const { repondre, ms, arg } = commandeOptions;
-
-  try {
-    // Caption for the screenshot
-    const cap = `*Screenshot taken by Lucky Md*`;
-
-    // Check if a URL is provided
-    if (!arg[0]) {
-      return repondre('Please insert a website link to take a screenshot!');
-    }
-
-    // Construct the screenshot URL
-    const image = `https://image.thum.io/get/maxAge/${arg[0]}`;
-
-    // Send the screenshot image with the caption
-    await zk.sendMessage(dest, {
-      image: { url: image },
-      caption: cap
-    }, { quoted: ms });
-
-  } catch (error) {
-    // Log the error and notify the user with a formatted error message
-    console.error(error);
-    repondre(`An error occurred while processing the screenshot: ${error.message}`);
-  }
-});
-zokou({
-  nomCom: "jpg",
-  categorie: "tak-screenshots",
-  reaction: "🎞️"
-}, async (dest, zk, commandeOptions) => {
-  const { repondre, ms, arg } = commandeOptions;
-
-  try {
-    // Caption for the screenshot
-    const cap = `*Screenshot taken by Lucky Md*`;
-
-    // Check if a URL is provided
-    if (!arg[0]) {
-      return repondre('Please insert a website link to take a screenshot!');
-    }
-
-    // Construct the screenshot URL
-    const image = `https://image.thum.io/get/allowJPG/${arg[0]}`;
-
-    // Send the screenshot image with the caption
-    await zk.sendMessage(dest, {
-      image: { url: image },
-      caption: cap
-    }, { quoted: ms });
-
-  } catch (error) {
-    // Log the error and notify the user with a formatted error message
-    console.error(error);
-    repondre(`An error occurred while processing the screenshot: ${error.message}`);
-  }
-});
-zokou({
-  nomCom: "png",
-  aliases: ["ss", "sshot"],
-  categorie: "take-screenshots",
-  reaction: "🎞️"
-}, async (dest, zk, commandeOptions) => {
-  const { repondre, ms, arg } = commandeOptions;
-
-  try {
-    // Caption for the screenshot
-    const cap = `*Screenshot taken by Lucky Md*`;
-
-    // Check if a URL is provided
-    if (!arg[0]) {
-      return repondre('Please insert a website link to take a screenshot!');
-    }
-
-    // Construct the screenshot URL
-    const image = `https://image.thum.io/get/png/${arg[0]}`;
-
-    // Send the screenshot image with the caption
-    await zk.sendMessage(dest, {
-      image: { url: image },
-      caption: cap
-    }, { quoted: ms });
-
-  } catch (error) {
-    // Log the error and notify the user with a formatted error message
-    console.error(error);
-    repondre(`An error occurred while processing the screenshot: ${error.message}`);
-  }
-});
-zokou({
-  nomCom: "noanimate",
-  categorie: "take-screenshots",
-  reaction: "🎞️"
-}, async (dest, zk, commandeOptions) => {
-  const { repondre, ms, arg } = commandeOptions;
-
-  try {
-    // Caption for the screenshot
-    const cap = `*Screenshot taken by Lucky Md*`;
+    const cap = `*Screenshot taken by Njabulo J *`;
 
     // Check if a URL is provided
     if (!arg[0]) {
