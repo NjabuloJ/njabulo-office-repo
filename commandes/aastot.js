8a664bf202d77b6ff4a3bc71106cd465a813d3d3
NjabuloJ <fanajb65@gmail.com>
2025-04-07 12:09:57 +0200

    Update aastot.js

--- commandes/aastot.js
+++ commandes/aastot.js
@@ -25,7 +25,7 @@ zokou({
     }
 
     // Construct playlist message
-    let playlistMessage = `LUCKY PLANET SPOTIFY PLAY\n\n`;
+    let playlistMessage = `NJABULO JB PLANET SPOTIFY PLAY\n\n`;
 
     // Loop through search results and construct track info with numbers
     searchData.forEach((track, index) => {
@@ -43,11 +43,17 @@ zokou({
       {
         text: playlistMessage,
         contextInfo: {
-          mentionedJid: [dest],  // Mention the sender's JID
+        isForwarded: true,
+         forwardedNewsletterMessageInfo: {
+         newsletterJid: '120363345407274799@newsletter',
+         newsletterName: "NJABULO JB",
+         serverMessageId: 143,
+         },
+         forwardingScore: 999, // Score to indicate it has been forwarded
           externalAdReply: {
             showAdAttribution: true,
-            title: "LUCKY MD SPOTIFY PLAY",
-            body: "Credit by frediezra",
+            title: "NJABULO JB SPOTIFY PLAY",
+            body: "Credit by Njabulo Jb",
             sourceUrl: "https://whatsapp.com/channel/0029VaihcQv84Om8LP59fO3f",
             mediaType: 1,
             renderLargerThumbnail: false,
